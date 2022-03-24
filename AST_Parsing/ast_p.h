bd3173e728c6efed259e548af2f7cc99b9280936
Souinia Ayoub <souiniaayoub@gmail.com>
2022-03-24 22:36:44 +0100

    start parsing test init ...

--- AST_Parsing/ast_p.h
+++ AST_Parsing/ast_p.h
@@ -6,7 +6,7 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 20:41:35 by asouinia          #+#    #+#             */
-/*   Updated: 2022/03/23 22:12:18 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/03/24 17:30:32 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,37 @@
 # include "../minishell.h"
 # include "../DoubleLinkedList/DoubleLinkedList.h"
 
-struct s_pipe_seq
-{
-	int	start;
-	int	end;
-	t_d_list *cmds;
-	t_d_list *p_ids;
-}		t_pipe_seq;
+//struct s_pipe_seq
+//{
+//	int	start;
+//	int	end;
+//	t_d_list *cmds;
+//	t_d_list *p_ids;
+//}		t_pipe_seq;
 
-struct s_cmd
-{
-	int	start;
-	int	end;
-	t_d_list *liter;
-	t_d_list *redi;
-}		t_cmd;
+//struct s_cmd
+//{
+//	int	start;
+//	int	end;
+//	t_d_list *liter;
+//	t_d_list *redi;
+//}		t_cmd;
 
-struct s_redi
-{
-	int	start;
-	int	end;
-	int 	type;
-	char	*file_limiter;
-}		t_redi;
+//struct s_redi
+//{
+//	int	start;
+//	int	end;
+//	int 	type;
+//	char	*file_limiter;
+//}		t_redi;
 
-struct s_literal
-{
-	int	start;
-	int	end;
-	char	*value;
-	char	*raw;
-	int		outquote;
-}		t_literal;
+//struct s_literal
+//{
+//	int	start;
+//	int	end;
+//	char	*value;
+//	char	*raw;
+//	int		outquote;
+//}		t_literal;
 
 #endif
--- handlers.c
+++ handlers.c
@@ -6,42 +6,117 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 17:01:53 by asouinia          #+#    #+#             */
-/*   Updated: 2022/03/23 14:42:40 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/03/24 22:35:37 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int parse_dquote(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line && line[i] && line[i] != '"')
+			i++;
+	return(i);
+}
+int parse_squote(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line && line[i] && line[i] != '\'')
+			i++;
+	return(i);
+}
+
+int	parse_pipe(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line && line[i] && line[i] != '|')
+	{
+		if (line[i] == '"')
+			i += parse_dquote(line + i + 1);
+		else if (line[i] == '\'')
+			i += parse_squote(line + i + 1);
+		i++;
+		//if (line[i] == '"' || line[i] == '\'')
+		//	i++;
+		printf("{%s}", line +i);
+	}
+	return(i);
+}
+
+int	parse (char *line)
+{
+	int i;
+
+	i = 0;
+	while (line && line[i] && line[i] != ' ')
+	{
+		if (line[i] == '"')
+			i += parse_dquote(line + i + 1);
+		else if (line[i] == '\'')
+			i += parse_squote(line + i + 1);
+		i++;
+		if (line[i] == '"' || line[i] == '\'')
+			i++;
+	}
+	return(i);
+}
+
 void	readinputs(char **read)
 {
 	char	*str;
-	char	**cmds;
-	char	**sss;
+	//char	**cmds;
+	//char	**sss;
 	int		i;
 	int		j;
+	int		k;
+	int		l;
+	char	*tmp;
 
 	str = *read;
 	while (1)
 	{
 		str = readline("minishell$ : ");
-		printf("=> %s\n", str);
-		printf("pipe checker %d\n", check_end_start_pipes(str));
-		printf("quote checker %d\n", quotescheckers(str, ft_strlen(str)));
-		printf("multipipes : %d\n", count_max_multiple_pipes(str));
-		cmds = NULL;
-		if (count_max_multiple_pipes(str) <= 1 && check_end_start_pipes(str))
-			cmds = split_cmds(str);
-		free(str);
-		i = -1;
-		while (cmds && cmds[++i])
+		//printf("==>==>==>==>\t%s\n", ft_strdup_v2(str, 0, parse(str)));
+		i = 0;
+		while (i < (int)ft_strlen(str))
 		{
-			printf("=>\t%s\n", cmds[i]);
-			j = -1;
-			sss = split_line(cmds[i]);
-			while (sss && sss[++j])
+			j = parse_pipe(str + i);
+			tmp = ft_strdup_v2(str, i, i + j - 1);
+			printf("==>\t%s\n", tmp);
+			k = 0;
+			while (k < (int)ft_strlen(tmp))
 			{
-				printf("========>\t%s\n", sss[j]);
+				l = parse(tmp + k);
+				printf("\t\t\t%s\n", ft_strdup_v2(tmp, k, k + l - 1));
+				k += l + 1;
 			}
+			i += j + 1;
 		}
+
+		//printf("pipe checker %d\n", check_end_start_pipes(str));
+		//printf("quote checker %d\n", quotescheckers(str, ft_strlen(str)));
+		//printf("multipipes : %d\n", count_max_multiple_pipes(str));
+		//cmds = NULL;
+		//if (count_max_multiple_pipes(str) <= 1 && check_end_start_pipes(str))
+		//	cmds = split_cmds(str);
+		free(str);
+		//i = -1;
+		//while (cmds && cmds[++i])
+		//{
+		//	printf("=>\t%s\n", cmds[i]);
+		//	j = -1;
+		//	sss = split_line(cmds[i]);
+		//	while (sss && sss[++j])
+		//	{
+		//		printf("========>\t%s\n", sss[j]);
+		//	}
+		//}
 	}
 }
