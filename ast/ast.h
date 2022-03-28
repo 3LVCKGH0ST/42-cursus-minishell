d61eea858d96dd1afd1b16f246b74c1a84ea9003
Souinia Ayoub <souiniaayoub@gmail.com>
2022-03-28 21:22:27 +0000

    ...

--- ast/ast.h
+++ ast/ast.h
@@ -6,7 +6,7 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 10:07:55 by asouinia          #+#    #+#             */
-/*   Updated: 2022/03/28 13:59:20 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/03/28 21:10:10 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,33 @@
 # include "./DoubleLinkedList.h"
 
 typedef enum e_type_node {
-	AST_LIST,
-	AST_REDIR,
-	AST_ID,
-	AST_TXT,
-	AST_SQOUTE,
-	AST_DQOUTE,
-	AST_PAREN,
-	AST_PIPELINE,
-	AST_AND,
-	AST_OR,
+	AST_LIST, // args and redirs : ls < abc > abc -la
+	AST_REDIR, // <<, >>, <, >
+	AST_ID, // abc"def"a abc abc'abc'a
+	AST_TXT, // abc
+	AST_SQOUTE,	// "abc"
+	AST_DQOUTE,	// 'abc'
+	AST_PAREN, // ( AST_LIST ) () (AST_OP) (AST_PIPELINE)
+	AST_PIPELINE, // AST_LIST | AST_LIST | AST_LIST ; AST_PAREN | AST_PAREN | AST_PAREN ;
+	AST_OP, // || &&;  AST_PIPELINE OP AST_PAREN ; AST_PIPELINE OP AST_PIPELINE  
 }	t_type_node;
 typedef struct s_ast
 {
 	t_type_node	type;
+
+	//! AST_LIST
+	t_d_list	*args; //? AST_ID AST_TXT AST_SQOUTE AST_DQOUTE
+	t_d_list	*redir; //? AST_REDIR
 	
-	t_d_list	*children; //ast_list
-	
+	//! AST_OP
+	t_ast	*left; //AST_PIPELINE AST_PAREN AST_LIST
+	t_ast	*right; //AST_PIPELINE AST_PAREN AST_LIST
+	//! AST_REDIR AST_ID AST_TXT AST_SQOUTE AST_DQOUTE AST_OP
 	t_e_token 	type_token;
+	//! AST_ID AST_TXT	AST_SQOUTE AST_DQOUTE AST_REDIR
 	char		*value;
-	
+	//! AST_ID 
+	t_d_list	*children; //? AST_TXT AST_SQOUTE AST_DQOUTE
 }	t_ast;
 
 t_ast	*init_ast(t_type_node type);
--- ast/parser.h
+++ ast/parser.h
@@ -6,7 +6,7 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 09:31:14 by asouinia          #+#    #+#             */
-/*   Updated: 2022/03/27 12:23:32 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/03/28 21:10:00 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,21 @@ typedef struct s_parser
 t_parser	*init_parser(t_lexer *lexer);
 t_token		*parser_advance(t_parser *parser);
 t_ast		*parse(t_parser *parser);
-t_ast		*parse_text(t_parser *parser);
-t_ast		*parse_string(t_parser *parser);
 //t_ast		*parse_(t_parser *parser);
 //t_ast		*parse(t_parser *parser);
+
+
+t_ast		*parse_text(t_parser *parser);
+t_ast		*parse_string(t_parser *parser);
+
+t_ast		*parse_arg(t_parser *parser);
+t_ast		*parse_redir(t_parser *parser);
+t_ast		*parse_list(t_parser *parser);
+t_ast		*parse_pipeline(t_parser *parser);
+t_ast		*parse_op(t_parser *parser);
+t_ast		*parse_paren(t_parser *parser);
+
+
+
 
 #endif
